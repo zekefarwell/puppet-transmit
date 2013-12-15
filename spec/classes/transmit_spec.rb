881c3c27e0e4696bc4cd83983011f215f2947422
ezekielf <ezekielf@users.noreply.github.com>
2013-12-15 15:20:47 -0500

    version 4.4.5

--- spec/classes/transmit_spec.rb
+++ spec/classes/transmit_spec.rb
@@ -4,7 +4,7 @@ describe 'transmit' do
   it do
     should contain_package('Transmit').with({
       :provider => 'compressed_app',
-      :source   => 'http://www.panic.com/transmit/d/Transmit%204.4.zip'
+      :source   => 'http://www.panic.com/transmit/d/Transmit%204.4.5.zip'
     })
   end
 end
